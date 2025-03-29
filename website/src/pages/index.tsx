a1b914c923b0318ad17fadd68196c35479124e35
Ziang LIU <50853393+zi-ang-liu@users.noreply.github.com>
2025-03-29 19:16:16 +0900

    a

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -5,6 +5,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import { FaResearchgate, FaGithub, FaUserCircle, FaGoogle, FaOrcid } from 'react-icons/fa';
 
 import styles from './index.module.css';
 
@@ -31,31 +32,58 @@ function HomepageHeader() {
 
 function TeamMembers() {
   const members = [
-    { name: '劉 子昂', role: '准教授', img: 'https://github.com/zi-ang-liu.png', website: 'https://zi-ang-liu.github.io/' },
-    { name: '法政太郎', role: '四年生', img: 'https://github.com/zi-ang-liu.png' },
-    { name: '法政花子', role: '三年生', img: 'https://github.com/zi-ang-liu.png' },
+    { name: '劉 子昂', role: '准教授', img: 'https://github.com/zi-ang-liu.png', link: 'https://zi-ang-liu.github.io/', github: 'https://github.com/zi-ang-liu/', researchgate: 'https://www.researchgate.net/profile/Ziang-Liu-4', google: 'https://scholar.google.com/citations?user=dRuC1OoAAAAJ&hl', orcid: 'https://orcid.org/0000-0002-1364-3502' },
+    { name: '学生', role: '四年生', img: 'https://github.com/zi-ang-liu.png' },
+    { name: '学生', role: '四年生', img: 'https://github.com/zi-ang-liu.png' },
+    { name: '学生', role: '四年生', img: 'https://github.com/zi-ang-liu.png' },
+    { name: '学生', role: '四年生', img: 'https://github.com/zi-ang-liu.png' },
+    { name: '学生', role: '三年生', img: 'https://github.com/zi-ang-liu.png' },
+    { name: '学生', role: '三年生', img: 'https://github.com/zi-ang-liu.png' },
+    { name: '学生', role: '三年生', img: 'https://github.com/zi-ang-liu.png' },
+    { name: '学生', role: '三年生', img: 'https://github.com/zi-ang-liu.png' },
+    { name: '学生', role: '三年生', img: 'https://github.com/zi-ang-liu.png' },
+    { name: '学生', role: '三年生', img: 'https://github.com/zi-ang-liu.png' },
   ];
 
   return (
     <section className={styles.teamMembers}>
       <h2>People</h2>
       <div className={clsx('row', styles.teamMembersRow)}>
-        {members.map(({ name, role, img, website }, index) => (
+        {members.map(({ name, role, img, link, github, researchgate, google, orcid }, index) => (
           <div key={index} className={clsx('col col--2', styles.teamMember)}>
             <div className={styles.card}>
               <img src={img} alt={name} className={styles.teamMemberImage} />
               <div className={styles.teamMemberContent}>
                 {/* make name link to homepage if website exist */}
-                <h3>
-                  {website ? (
-                    <Link to={website} className={styles.teamMemberLink}>
-                      {name}
-                    </Link>
-                  ) : (
-                    name
-                  )}
-                </h3>
+                <h3>{name}</h3>
                 <p>{role}</p>
+                <div className={styles.socialLinks}>
+                  {link && (
+                    <Link to={link} className={styles.socialLink}>
+                      <FaUserCircle />
+                    </Link>
+                  )}
+                  {github && (
+                    <Link to={github} className={styles.socialLink}>
+                      <FaGithub />
+                    </Link>
+                  )}
+                  {google && (
+                    <Link to={google} className={styles.socialLink}>
+                      <FaGoogle />
+                    </Link>
+                  )}
+                  {orcid && (
+                    <Link to={orcid} className={styles.socialLink}>
+                      <FaOrcid />
+                    </Link>
+                  )}
+                  {researchgate && (
+                    <Link to={researchgate} className={styles.socialLink}>
+                      <FaResearchgate />
+                    </Link>
+                  )}
+                </div>
               </div>
             </div>
           </div>
