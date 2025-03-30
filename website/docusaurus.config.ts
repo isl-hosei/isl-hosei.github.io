878ea8595898f9dc269b5cc7f1209204b7316dcf
Ziang LIU <50853393+zi-ang-liu@users.noreply.github.com>
2025-03-30 11:08:29 +0900

    Update docusaurus.config.ts

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -41,8 +41,8 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://isl-hosei/isl-hosei.github.io/',
+          // editUrl:
+          //   'https://isl-hosei/isl-hosei.github.io/',
         },
         blog: {
           showReadingTime: true,
@@ -52,8 +52,8 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://isl-hosei/isl-hosei.github.io/',
+          // editUrl:
+          //   'https://isl-hosei/isl-hosei.github.io/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
