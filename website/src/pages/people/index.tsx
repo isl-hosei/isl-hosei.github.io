4d8cd8a40d3dc6db40e81267f1dea5ade6c2dd11
Ziang LIU <50853393+zi-ang-liu@users.noreply.github.com>
2025-03-31 23:28:13 +0900

    a

--- website/src/pages/people/index.tsx
+++ website/src/pages/people/index.tsx
@@ -8,6 +8,7 @@ import { FaResearchgate, FaGithub, FaUserCircle, FaGoogle, FaOrcid } from 'react
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
+import { members } from './data/members';
 
 function HomepageHeader() {
     const { siteConfig } = useDocusaurusContext();
@@ -19,30 +20,6 @@ function HomepageHeader() {
         </header >
     );
 }
-
-const members = [
-    {
-        name: '劉 子昂',
-        role: '准教授',
-        img: 'https://github.com/zi-ang-liu.png',
-        link: 'https://zi-ang-liu.github.io/',
-        github: 'https://github.com/zi-ang-liu/',
-        researchgate: 'https://www.researchgate.net/profile/Ziang-Liu-4',
-        google: 'https://scholar.google.com/citations?user=dRuC1OoAAAAJ&hl',
-        orcid: 'https://orcid.org/0000-0002-1364-3502'
-    },
-
-    { name: '学生', role: '四年生', img: 'https://github.com/isl-hosei.png' },
-    { name: '学生', role: '四年生', img: 'https://github.com/isl-hosei.png' },
-    { name: '学生', role: '四年生', img: 'https://github.com/isl-hosei.png' },
-    { name: '学生', role: '四年生', img: 'https://github.com/isl-hosei.png' },
-    { name: '学生', role: '三年生', img: 'https://github.com/isl-hosei.png' },
-    { name: '学生', role: '三年生', img: 'https://github.com/isl-hosei.png' },
-    { name: '学生', role: '三年生', img: 'https://github.com/isl-hosei.png' },
-    { name: '学生', role: '三年生', img: 'https://github.com/isl-hosei.png' },
-    { name: '学生', role: '三年生', img: 'https://github.com/isl-hosei.png' },
-    { name: '学生', role: '三年生', img: 'https://github.com/isl-hosei.png' },
-];
 
 function TeamMembers() {
 
