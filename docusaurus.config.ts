614c90ac61e7e526916eedd2eed1f19e78901cd2
Ziang <liu.ziang@outlook.com>
2025-05-20 14:12:23 +0900

    Create docusaurus.config.ts

---
+++ docusaurus.config.ts
@@ -0,0 +1,149 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Intelligent Systems Laboratory',
+  tagline: '法政大学 理工学部 経営システム工学科',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://isl-hosei.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'isl-hosei', // Usually your GitHub org/user name.
+  projectName: 'isl-hosei.github.io', // Usually your repo name.
+  trailingSlash: false,
+  deploymentBranch: 'gh-pages',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://isl-hosei/isl-hosei.github.io/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://isl-hosei/isl-hosei.github.io/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: '知能システム研究室',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/favicon.ico',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'right',
+          label: 'Docs',
+        },
+        { to: 'people', label: 'People', position: 'right' },
+        { to: 'publications', label: 'Publications', position: 'right' },
+        { to: 'join', label: 'Join Us', position: 'right' },
+        { to: '/blog', label: 'Blog', position: 'right' },
+        {
+          href: 'https://github.com/isl-hosei',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        // {
+        //   title: 'Docs',
+        //   items: [
+        //     {
+        //       label: 'Tutorial',
+        //       to: '/docs/intro',
+        //     },
+        //   ],
+        // },
+        {
+          title: 'Links',
+          items: [
+            {
+              label: '法政大学',
+              href: 'https://www.hosei.ac.jp',
+            },
+            {
+              label: '経営システム工学科',
+              href: 'https://ise-hp.ws.hosei.ac.jp',
+            },
+            {
+              label: 'IIST',
+              href: 'https://iist.hosei.ac.jp',
+            },
+          ],
+        },
+        // {
+        //   title: 'More',
+        //   items: [
+        //     {
+        //       label: 'Blog',
+        //       to: '/blog',
+        //     },
+        //     {
+        //       label: 'GitHub',
+        //       href: 'https://github.com/facebook/docusaurus',
+        //     },
+        //   ],
+        // },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Intelligent Systems Laboratory, Hosei University.`,
+      // Built with Docusaurus.
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
